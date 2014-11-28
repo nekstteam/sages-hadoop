9fa515a40c31e299cb700e20bb0b4e587e6c497b
Radosław Szmit <radoslawszmit@gmail.com>
2014-11-28 16:09:09 +0100

    Hbase jdo

---
+++ hbase/hbase-jdo/src/test/java/org/datanucleus/samples/jdo/tutorial/HbaseDatanucleusJdoTest.java
@@ -0,0 +1,40 @@
+package org.datanucleus.samples.jdo.tutorial;
+
+import org.junit.Test;
+
+import javax.jdo.JDOHelper;
+import javax.jdo.PersistenceManager;
+import javax.jdo.PersistenceManagerFactory;
+import javax.jdo.Transaction;
+
+public class HbaseDatanucleusJdoTest {
+
+    @Test
+    public void shouldSaveEntity() throws Exception {
+        //given
+
+        PersistenceManagerFactory pmf = JDOHelper.getPersistenceManagerFactory("Tutorial");
+        PersistenceManager pm = pmf.getPersistenceManager();
+
+        Transaction tx = pm.currentTransaction();
+        try {
+            tx.begin();
+            Inventory inv = new Inventory("My Inventory");
+            Product product = new Product("Sony Discman", "A standard discman from Sony", 49.99);
+            inv.getProducts().add(product);
+            pm.makePersistent(inv);
+            tx.commit();
+        } finally {
+            if (tx.isActive()) {
+                tx.rollback();
+            }
+            pm.close();
+        }
+
+
+        //when
+
+        //then
+
+    }
+}
