e6fc9cb27c5cafcefdbfb5dbb92be09c01be6c30
btartanus <bartektartanus@gmail.com>
2014-11-30 18:28:35 +0100

    zadania z mahouta dodane

--- mahout/src/main/java/pl/com/sages/mahout/recommendation/Ex4.java
+++ mahout/src/main/java/pl/com/sages/mahout/recommendation/Ex4.java
@@ -16,27 +16,36 @@ import java.net.URL;
 import java.util.List;
 
 /**
- * Created by btartanus on 22.11.14.
- */
-public class Recommendation {
+ * Created by btartanus on 30.11.14.
+ *
+ *
+ * Exercise 4
+ • Create a DataModel
+ • Feed the DataModel through a CSV file
+ • Calculate similarities between users
+ – CSV file should contain enough data!
+ • Generate neighboorhood
+ • Generate recommendations
+ – Compare different combinations of parameters!
+ • Hints about objects to be used:
+ – NearestNUserNeighborhood
+ – GenericUserBasedRecommender
+ • Parameters: data model, neighborhood, similarity measure
 
+ */
+public class Ex4 {
     public static void main(String[] args) throws Exception {
         URL url = EvaluatorIntro.class.getClassLoader().getResource("intro.csv");
         URI uri = url.toURI();
         DataModel model = new FileDataModel(new File(uri));
-
         UserSimilarity similarity = new PearsonCorrelationSimilarity(model);
-        UserNeighborhood neighborhood =
-                new NearestNUserNeighborhood(2, similarity, model);
-
+        UserNeighborhood neighborhood = new NearestNUserNeighborhood(2, similarity, model);
         Recommender recommender = new GenericUserBasedRecommender(
                 model, neighborhood, similarity);
-
-        List<RecommendedItem> recommendations = recommender.recommend(1, 3);
-
+        List<RecommendedItem> recommendations = recommender.recommend(1, 1);
         for (RecommendedItem recommendation : recommendations) {
             System.out.println(recommendation);
         }
-
     }
+
 }
--- mahout/src/main/java/pl/com/sages/mahout/recommendation/Ex5.java
+++ mahout/src/main/java/pl/com/sages/mahout/recommendation/Ex5.java
@@ -16,27 +16,31 @@ import java.net.URL;
 import java.util.List;
 
 /**
- * Created by btartanus on 22.11.14.
- */
-public class Recommendation {
+ * Created by btartanus on 30.11.14.
+ *
+ * Exercise 5: MovieLens Recommender
+ • Download the GroupLens dataset (100k)
+ – Its format is already Mahout compliant
+ – http://files.grouplens.org/datasets/movielens/ml-100k.zip
+ • Preparatory Exercise: repeat exercise 3 (similarity
+ calculations) with a bigger dataset
+ • Next: now we can run the recommendation
+ framework against a state-of-the-art dataset
 
+ */
+public class Ex5 {
     public static void main(String[] args) throws Exception {
-        URL url = EvaluatorIntro.class.getClassLoader().getResource("intro.csv");
+        URL url = EvaluatorIntro.class.getClassLoader().getResource("ua.base");
         URI uri = url.toURI();
         DataModel model = new FileDataModel(new File(uri));
-
         UserSimilarity similarity = new PearsonCorrelationSimilarity(model);
-        UserNeighborhood neighborhood =
-                new NearestNUserNeighborhood(2, similarity, model);
-
+        UserNeighborhood neighborhood = new NearestNUserNeighborhood(100, similarity, model);
         Recommender recommender = new GenericUserBasedRecommender(
                 model, neighborhood, similarity);
-
-        List<RecommendedItem> recommendations = recommender.recommend(1, 3);
-
+        List<RecommendedItem> recommendations = recommender.recommend(1, 20);
         for (RecommendedItem recommendation : recommendations) {
             System.out.println(recommendation);
         }
-
     }
+
 }
