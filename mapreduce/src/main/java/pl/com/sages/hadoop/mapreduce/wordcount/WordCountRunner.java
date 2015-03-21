f2da9bbba69d1cd772d18a51b95243a5b032e6ac
sages <biuro@sages.com.pl>
2015-03-21 15:26:36 +0000

    Hadoop word count

---
+++ mapreduce/src/main/java/pl/com/sages/hadoop/mapreduce/wordcount/WordCountRunner.java
@@ -0,0 +1,34 @@
+package pl.com.sages.hadoop.mapreduce.wordcount;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
+public class WordCountRunner {
+
+    public static void main(String[] args) throws Exception {
+
+        Path inputPath = new Path(args[0]);
+        System.out.println(inputPath);
+
+        Path outputPath = new Path(args[1]);
+        System.out.println(outputPath);
+
+        Configuration conf = new Configuration();
+        Job job = Job.getInstance(conf, "word count");
+        job.setJarByClass(WordCountRunner.class);
+        job.setMapperClass(WordCountMapper.class);
+        job.setCombinerClass(WordCountReducer.class);
+        job.setReducerClass(WordCountReducer.class);
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(IntWritable.class);
+        FileInputFormat.addInputPath(job, inputPath);
+        FileOutputFormat.setOutputPath(job, outputPath);
+        System.exit(job.waitForCompletion(true) ? 0 : 1);
+    }
+
+}
