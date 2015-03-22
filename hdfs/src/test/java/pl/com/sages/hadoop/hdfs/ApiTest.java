930c91ae7d3275361434cdc884e5745416c95ff5
sages <biuro@sages.com.pl>
2015-03-22 23:01:33 +0000

    Czytanie pliku

--- hdfs/src/test/java/pl/com/sages/hadoop/hdfs/ApiTest.java
+++ hdfs/src/test/java/pl/com/sages/hadoop/hdfs/ApiTest.java
@@ -1,13 +1,17 @@
 package pl.com.sages.hadoop.hdfs;
 
 import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FSDataInputStream;
 import org.apache.hadoop.fs.FileStatus;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.hdfs.DistributedFileSystem;
+import org.apache.hadoop.io.IOUtils;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
@@ -41,7 +45,8 @@ public class ApiTest {
 
         List<String> files = new ArrayList<>();
         for (FileStatus fileStatuse : fileStatuses) {
-            files.add(fileStatuse.getPath().getName());
+            String filename = fileStatuse.getPath().getName();
+            files.add(filename);
         }
 
         // then
@@ -53,10 +58,25 @@ public class ApiTest {
     @Test
     public void shouldReadFile() throws Exception {
         // given
+        String inputPath = "/demo/data/Customer/acct.txt";
+        String outputPath = "/tmp/acct.txt";
+
+        new File(outputPath).delete();
+        FSDataInputStream inputStream = fs.open(new Path(inputPath));
+
+        // przewijanie do przodu
+//        inputStream.seek(2000);
 
         // when
+        try {
+//        IOUtils.copyBytes(inputStream, System.out, 4096);
+            IOUtils.copyBytes(inputStream, new FileOutputStream(new File(outputPath)), 4096);
+        } finally {
+            IOUtils.closeStream(inputStream);
+        }
 
         // then
-
+        assertThat(new File(outputPath)).exists();
     }
+
 }
