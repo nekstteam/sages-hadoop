97db2d2978249b62b225282b15fbb11083e4b12d
Radosław Szmit <radoslawszmit@gmail.com>
2015-03-24 06:10:43 +0100

    Hive

--- hive/queries.sql
+++ hive/queries.sql
@@ -20,3 +20,27 @@ SELECT count(DISTINCT userid),count(DISTINCT movieid) FROM ratings;
 select movieid, title, split(genres,"\\|") AS genre from movies limit 10;
 
 
+SELECT userid, movieid,
+ CASE
+  WHEN rating < 1 THEN 'bad'
+  WHEN rating >= 1 AND rating < 3 THEN 'ok'
+  WHEN rating >= 3 AND rating < 5 THEN 'good'
+  ELSE 'very good'
+END AS human_rating
+FROM ratings limit 100;
+
+select * from ratings where rating >= 3 limit 100;
+
+
+select * from movies where title like 'Toy%' limit 10;
+
+select * from movies where title rlike '.*(Hulk|Spiderman|Avengers|Batman).*';
+
+
+select avg(rating) from ratings;
+select userid, avg(rating) from ratings GROUP BY userid limit 10;
+select userid, avg(rating) from ratings GROUP BY userid HAVING avg(rating) >= 4 limit 10;
+
+
+
+
